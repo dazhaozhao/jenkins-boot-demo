1d70f577a70e1c773eb823c326d6bf63af42f474
zhaohaibo <zhaohaibo98@126.com>
2023-02-16 10:42:18 +0800

    init

---
+++ src/main/java/com/example/jenklinsbootdemo/JenkinsBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenklinsbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsBootDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jenklinsbootdemo/JenkinsBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenklinsbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsBootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
